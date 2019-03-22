c6004485adecba24cc3a47739622188836638a07
zhaobao1830 <x5219438>
2019-03-22 13:33:34 +0800

    header完成

--- src/pages/components/header.vue
+++ src/pages/components/header.vue
@@ -9,7 +9,7 @@
     </div>
     <div class="header-right">
       城市
-      <span class="iconfont">&#xe64a;</span>
+      <span class="iconfont arrow-icon">&#xe64a;</span>
     </div>
   </div>
 </template>
@@ -21,11 +21,13 @@
 </script>
 
 <style lang="stylus" scoped>
+  @import "~styles/varibles"
+
   .header
     height: .86rem
     display: flex
     align-items: center
-    background: #00bcd4
+    background: $bgColor
     color: #fff
     .header-left
       flex: 0 0 .64rem
@@ -37,9 +39,13 @@
       border-radius: .1rem
       height: .64rem
       margin-left: .2rem
+      padding-left: .2rem
       color: #ccc
       line-height: .64rem
       text-align: left
     .header-right
       flex: 0 0 1.24rem
+      .arrow-icon
+        margin-left: -.04rem
+        font-size: .24rem
 </style>
