f5da02f4b0c135eeba0314edd1ec7694496a5a80
zhaobao1830 <x5219438>
2019-03-26 08:32:12 +0800

    热销推荐组件完成

--- src/pages/components/recommend.vue
+++ src/pages/components/recommend.vue
@@ -2,13 +2,13 @@
   <div>
     <div class="title">热销推荐</div>
     <ul>
-      <li class="item border-bottom">
+      <li class="item border-bottom" v-for="item of recommendList" :key="item.id">
         <div class="item-img-wrapper">
-          <img class="item-img" src="http://img1.qunarzz.com/sight/p0/1901/ed/ed4079d797a78d28a3.water.jpg_200x200_53b1d509.jpg">
+          <img class="item-img" :src="item.imgUrl">
         </div>
         <div class="item-info">
-          <p class="item-title">榆次老城</p>
-          <p class="item-desc">榆次老城</p>
+          <p class="item-title">{{item.title}}</p>
+          <p class="item-desc">{{item.desc}}</p>
           <button class="item-button">查看详情</button>
         </div>
       </li>
@@ -18,11 +18,43 @@
 
 <script>
   export default {
-    name: 'HomeRecommend'
+    name: 'HomeRecommend',
+    data () {
+      return {
+        recommendList: [
+          {
+            id: '0001',
+            imgUrl: 'http://img1.qunarzz.com/sight/p0/1901/ed/ed4079d797a78d28a3.water.jpg_200x200_53b1d509.jpg',
+            title: '榆次老城',
+            desc: '榆次老城欢迎您'
+          },
+          {
+            id: '0002',
+            imgUrl: 'http://img1.qunarzz.com/sight/p0/1901/ed/ed4079d797a78d28a3.water.jpg_200x200_53b1d509.jpg',
+            title: '榆次老城',
+            desc: '榆次老城欢迎您'
+          },
+          {
+            id: '0003',
+            imgUrl: 'http://img1.qunarzz.com/sight/p0/1901/ed/ed4079d797a78d28a3.water.jpg_200x200_53b1d509.jpg',
+            title: '榆次老城',
+            desc: '榆次老城欢迎您'
+          },
+          {
+            id: '0004',
+            imgUrl: 'http://img1.qunarzz.com/sight/p0/1901/ed/ed4079d797a78d28a3.water.jpg_200x200_53b1d509.jpg',
+            title: '榆次老城',
+            desc: '榆次老城欢迎您'
+          }
+        ]
+      }
+    }
   }
 </script>
 
 <style scoped lang="stylus" rel="stylesheet/stylus">
+  @import "~styles/mixins.styl"
+
   .title
     margin-top: .2rem
     line-height: .8rem
@@ -41,14 +73,20 @@
       flex: 1
       padding: .1rem
       text-align: left
+      min-width: 0
       .item-title
         line-height: .54rem
         font-size: .32rem
+        ellipsis()
       .item-desc
         line-height: .4rem
         color: #ccc
+        ellipsis()
       .item-button
+        line-height: .44rem
+        margin-top .16rem
         background: #ff9300
-        padding: 0 .1rem
+        padding: 0 .2rem
         border-radius: .06rem
+        color: #fff
 </style>
