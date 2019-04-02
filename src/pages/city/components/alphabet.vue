30a36f38c4cd6c275ff2f2d71cb92dbee50137c5
zhaobao1830 <x5219438>
2019-04-02 13:28:11 +0800

    城市组件页面，右侧滑动，左侧改变

--- src/pages/city/components/alphabet.vue
+++ src/pages/city/components/alphabet.vue
@@ -2,11 +2,14 @@
   <ul class="list">
     <li
       class="item"
-      v-for="(item, key) of cities"
-      :key="key"
+      v-for="item of letters"
+      :key="item"
       @click="handleletterClick"
+      @touchstart="handleTouchStart"
+      @touchmove="handleTouchMove"
+      @toucend="handleTouchEnd"
     >
-      {{key}}
+      {{item}}
     </li>
   </ul>
 </template>
@@ -20,9 +23,39 @@
         default: () => {}
       }
     },
+    data () {
+      return {
+        touchStatus: false
+      }
+    },
+    computed: {
+      letters() {
+        const letters = []
+        for (let i in this.cities) {
+          letters.push(i)
+        }
+        return letters
+      }
+    },
     methods: {
       handleletterClick (e) {
         this.$emit('change', e.target.innerText)
+      },
+      handleTouchStart () {
+        this.touchStatus = true
+      },
+      handleTouchMove (E) {
+        if (this.touchStatus) {
+          const startY = this.$refs['A'][0].offsetTop
+          const touchY = e.touches[0].clientY - 79
+          const index = Math.floor((touchY - startY) / 20)
+          if (index >= 0 && index < this.letters.length) {
+            this.$emit('change', this.letters[index])
+          }
+        }
+      },
+      handleTouchEnd () {
+        this.touchStatus = false
       }
     }
   }
